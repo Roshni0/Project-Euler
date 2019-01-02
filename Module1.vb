32b62698cd4114ed7bf2a6741f9587de312f2d2b
Roshni0 <44888386+Roshni0@users.noreply.github.com>
2019-01-02 09:20:02 +0800

    Add files via upload

---
+++ Module1.vb
@@ -0,0 +1,21 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim sum As Integer = 0
+        Dim x As Integer = 1
+        Dim y As Integer = 2
+        Dim temp As Integer = 0
+        Do While x < 4000000
+            If x Mod 2 = 0 Then
+                sum = sum + x
+            ElseIf y Mod 2 = 0 Then
+                sum = sum + y
+            End If
+            x = x + y
+            y = y + x
+        Loop
+        Console.WriteLine(sum)
+        Console.ReadLine()
+    End Sub
+
+End Module
