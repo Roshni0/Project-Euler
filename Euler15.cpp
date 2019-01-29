ab7e5130cd69cb90e50bcf1404fc6f3edddf3cf9
Roshni Vachhani <Roshni.Vachhani@tts.edu.sg>
2019-01-29 21:33:53 +0800

    Add files via upload

---
+++ Euler15.cpp
@@ -0,0 +1,13 @@
+// Euler14.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include <iostream>
+
+using namespace std;
+int main()
+{
+//This doesn't require a code
+//Just a calculator!
+//(40!)/(20!20!)
+}
