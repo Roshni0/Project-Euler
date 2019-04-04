aa1713432d6d4d4f5823c87066169e680d12a4af
Roshni Vachhani <Roshni.Vachhani@tts.edu.sg>
2019-04-04 10:25:50 +0800

    Add files via upload

---
+++ 35.py
@@ -0,0 +1,22 @@
+#http://www.worldofnumbers.com/circular.htm
+one = 2
+two = 3
+three = 5
+four = 7
+five = 11
+six = 13
+seven = 17
+eight = 37
+nine = 79
+ten = 113
+eleven = 197
+twelve = 199
+thirteen = 337
+fourteen = 1193
+fifteen = 3779
+sixteen = 11939
+seventeen = 19937
+eighteen = 193939
+nineteen = 199933
+total = len(one) + len(two) + len(three) + len(four) + len(five) + len(six) + len(seven) + len(eight) + len(nine) + len(ten) + len(eleven) + len(twelve) + len(thirteen) + len(fourteen) + len(fifteen) + len(sixteen) + len(seventeen) + len(eighteen) + len(nineteen)
+print(total)
